5a6b2e28d5c1de63c55186217907fbaa5f856b33
jbmacdonald1012 <jbmacdonald1012@gmail.com>
2025-06-24 22:33:51 -0600

    Week 04 Complete.

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { Document } from '../document.model';
 
 @Component({
@@ -8,10 +8,16 @@ import { Document } from '../document.model';
   styleUrl: './document-list.component.css'
 })
 export class DocumentListComponent {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
 
   documents: Document[] = [
-    new Document('1', 'Doc A', 'Description A', 'http://example.com/a', []),
-    new Document('2', 'Doc B', 'Description B', 'http://example.com/b', []),
-    new Document('3', 'Doc C', 'Description C', 'http://example.com/c', [])
+    new Document('1', 'Doc A', 'This is Document A', 'http://example.com/a', []),
+    new Document('2', 'Doc B', 'This is Document B', 'http://example.com/b', []),
+    new Document('3', 'Doc C', 'This is Document C', 'http://example.com/c', []),
+    new Document('4', 'Doc D', 'This is Document D', 'http://example.com/d', []),
   ];
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
+  }
 }
